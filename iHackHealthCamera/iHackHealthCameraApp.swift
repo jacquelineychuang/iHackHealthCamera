ede3bf7dc289a08aeb11a12b347ca9f9af051fb9
jacquelineychuang <jacquelineychuang@gmail.com>
2022-01-31 22:13:33 -0800

    Initial Commit

---
+++ iHackHealthCamera/iHackHealthCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  iHackHealthCameraApp.swift
+//  iHackHealthCamera
+//
+//  Created by Laura Ke on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct iHackHealthCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
